94268312a8438498c408eedfbfbd699db01dd815
Dr. Holomorfo <holomorfo.mx@gmail.com>
2023-10-27 20:37:45 -0700

    jsdom config

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   modulePathIgnorePatterns: ["dist"],
   collectCoverage: true,
-  coverageDirectory: "coverage_2",
+  coverageDirectory: "coverage",
 };
